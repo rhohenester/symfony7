1bde9e5e9b9766316fb8b8d72fc70635d4c6d292
rhohenester <raissa@DESKTOP-A401TGM>
2024-04-16 14:52:21 +0200

    Adjustments to routes and 404

--- src/Repository/StarshipRepository.php
+++ src/Repository/StarshipRepository.php
@@ -38,4 +38,15 @@ class StarshipRepository
             ),
         ];
     }
+
+    public function find(int $id): ?Starship
+    {
+        foreach ($this->findAll() as $starship) {
+            if ($starship->getId() === $id) {
+                return $starship;
+            }
+            
+        }
+        return null;
+    }
 }
